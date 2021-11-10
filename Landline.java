e1604b43fdc9c6428f0a056ea62ec9dc52980957
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2021-11-09 23:55:11 -0600

    Add files via upload

--- Landline.java
+++ Landline.java
@@ -1,9 +1,22 @@
 public class Landline extends Device implements Llamadas {
     
     private String device= "Landline";
-    
+    public Landline(String serialNumber, String manufacturer, String productionDate, String markerAR, String deviceType){
+        super(serialNumber, manufacturer, productionDate, markerAR, deviceType);
+    }
     public void Llamar(int num){
         String p="LLamando al numero"+num+"desde"+device; 
         System.out.print(p);
 }
+    
+    public void Funcionalidades(){
+        String Funcionalidades = """
+                                                
+                                                1. LLamar
+                                                9. Comprar
+                                                
+                                """
+                ;
+        System.out.println(Funcionalidades);
+    }
 }
